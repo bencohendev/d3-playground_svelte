235cf14b81c39d3f4de9448d222c81c30097b3b1
bencohendev <ben@bencohen.dev>
2021-02-17 11:59:48 -0500

    fixed color completely

--- src/helpers/GetNodeColor.svelte
+++ src/helpers/GetNodeColor.svelte
@@ -1,7 +1,7 @@
 <script context="module">
     export function getNodeColor(node, neighbors) {
         if (typeof neighbors === "object") {
-            if (neighbors.indexOf(node.id) > 0) {
+            if (neighbors.indexOf(node.id) >= 0) {
                 return node.level === 1 ? "blue" : "green";
             }
         }
